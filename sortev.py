36cbfa81bfc49fb2ff8feac32099a28efe7d27eb
drbtx1 <drbtx1@users.noreply.github.com>
2024-05-08 14:11:43 -0400

    Add files via upload

--- sortev.py
+++ sortev.py
@@ -12,23 +12,40 @@ import numpy as np
 form the eigenvectors) and the same structure as a list of sorted eigenvectors, 
 and returns the list sorted in the same order as the presorted list. This 
 assumes that two lists are only incrementally different and there is some  
-metric to determine how similar the vectors in each set are to each other'''
+metric (inner product is used here) to determine how similar the vectors in each set are to each other'''
 
 def sortEigenvectors(newList, oldList):
     #transpose to simplify indexing
-    newListT = newList.T
-    oldListT = oldList.T
+    newListT = columntorow(newList)
+    oldListT = columntorow(oldList)
     
     #take advantage of multiplicity - sorted by (degenerate) eigenvalues
     #in other parts of code
     
     N = len(newList)
-    counter = 0             
-    
-    for element in distribution(N):
-        subset = newListT[counter : counter + distribution[element]]
-        counter = counter + distribution[element]
-        for eigenvector in subset:
+    counter = 0      
+    #calculate multiplicity of degenerate eigenvalues   
+    ways = distribution(N)
+    for element in ways:
+        element = int(element)
+        print(element)
+        sortedEigenvectorsT = []    
+        for eigenvector in range(counter, counter + element):
+            bestfit = eigenvector
+            print("best = " + str(bestfit))
+            bestfitInnerProduct = np.inner(oldListT[eigenvector], newListT[bestfit])
+            #could do a swap in newListT instead of appending to another list to avoid double checking
+            for eigenvectortocompare in range(counter, counter + element):
+                tempinner = np.inner(oldListT[eigenvector], newListT[eigenvectortocompare])
+                if tempinner > bestfitInnerProduct:
+                    bestfitInnerProduct = tempinner
+                    bestfit = eigenvectortocompare
+            sortedEigenvectorsT.append(newListT[bestfit]) 
+            counter = counter + element
+    #transpose to return to column format used by numpy for eigenvalues         
+    return(columntorow(sortedEigenvectorsT))        
+                
+         
             
     
     
@@ -38,11 +55,28 @@ of binomial distribution, prob(N) = prob(0)'''
 def distribution(N):
     W = []
     for n in range(0,N+1):
-        print(n)
         W.append(np.math.factorial(N)/(np.math.factorial(N-n)*np.math.factorial(n)))
     return W    
 
-print(distribution(4))
+#assumes square list of lists
+def columntorow(A):
+    length = len(A)
+    B = []
+    for column in range(0,length):
+        temp = []
+        for row in range(0,length):
+            temp.append(A[row][column])
+        B.append(temp)
+    print(B)    
+    return B    
+            
+        
+
+
+newList = [[1,0,0]]
+oldList = [[1.01,0,0]]
+
+print(sortEigenvectors(newList,oldList))
     
     
     
