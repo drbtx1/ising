488100617b455ec49ac7b0ccff72360da35792df
drbtx1 <drbtx1@users.noreply.github.com>
2025-03-09 11:24:29 -0400

    Add files via upload

--- QICFB.py
+++ QICFB.py
@@ -10,6 +10,9 @@ import scipy as sp
 
 c = np.array([[0,1],[0,0]])
 cplus = np.array([[0,0],[1,0]])
+sx = np.array([[0,1],[1,0]])
+sy = np.array([[0,-1j],[1j,0]])
+sz = np.array([[1,0],[0,-1]])
 
 periodic = False
 
@@ -119,7 +122,7 @@ def ccplusMatrixPBC(n):
 def hTerm(n):
     operator = np.eye(2**n)
     count = fermionWeights(n)
-    print(count)
+    #print(count)
     for index in range(2**n):
         term = index
         sum = 0
@@ -214,8 +217,128 @@ def HfullPBC25(n):
             H[row][column] = odd[row- 2**(n-1)][column - 2**(n-1)] 
     return H
 
-print(HfullPBC25(4))        
+#print(HfullPBC25(4))
+
+
+def find_k_odd(L):
+    narray = []
+    n = -L/2 + 1
+    while (n < L/2):
+        narray.append(n)
+        n += 1/2
+    narray.append(n)    
+    k = []
+    multiple = 2*np.pi/L
+    for idx in range(len(narray)):
+        k.append(multiple*narray[idx])
+    k.sort()
+    return k
+
+
+
+def find_k_even(L):
+    k = []
+    #multiple = np.pi/L
+    n = 1
+    while((2*n - 1) <= (L - 1) ):
+        k.append((2*n - 1)*np.pi/L)
+        k.append(-1*(2*n - 1)*np.pi/L)
+        n = n + 1
+    #k.append(n*multiple)
+    k.sort()
+    return k        
             
-               
+
+
+phi = 0
+kappa = 0.5
+def ck(n,k):
+    sum = 0
+    for j in range(n):
+        operators = []
+        for index in range(n):
+            operators.append((np.eye(2)))
+        operators[j] = c
+        product = operators[0]
+        for index in range(1,n):
+            product = np.kron(product, operators[index])
+        sum += np.exp(-1j*k*j)*product
+    return np.exp(-1j*phi)*sum/np.sqrt(n)
+
+def cplusk(n,k):
+    sum = 0
+    for j in range(n):
+        operators = []
+        for index in range(n):
+            operators.append((np.eye(2)))
+        operators[j] = cplus
+        product = operators[0]
+        for index in range(1,n):
+            product = np.kron(product, operators[index])
+        sum += np.exp(1j*k*j)*product
+    return np.exp(1j*phi)*sum/np.sqrt(n)
+
+def nk0(n):
+    return cplusk(n,0)@ck(n,0)
+
+def nkpi(n):
+    return cplusk(n,np.pi)@ck(n,np.pi)
+
+
+
+def Kpluseven(n):
+    k = []
+    idx = 1
+    while idx <= n/2 :
+        k.append((2*idx-1)*np.pi/n)
+        idx += 1
+    return k
+
+
+def Kplusodd(n):
+    k = np.array(find_k_odd(n))
+    k =k[k > 0]
+    k = k[:-1]
+    return k
+            
+
+#print(Kpluseven(10))
+def Hk0pi(n):
+    return -2*J*(nk0(n) - nkpi(n)) + 2*h*(nk0(n) + nkpi(n) - np.eye(2**n))
+#print(Hk0pi(3))    
+
+def Hkspace(n,k):
+    return 2*(h - J*np.cos(k))*(cplusk(n,k)@ck(n,k) - ck(n,-k)@cplusk(n,-k))\
+    -2*kappa*J*np.sin(k)*(1j*np.exp(-2*1j*phi)*cplusk(n,k)@cplusk(n,-k)\
+    - 1j*np.exp(2*1j*phi)*ck(n, -k)@ck(n,k))
+        
+def Hkspaceeven(n):
+    sum = np.zeros((2**n, 2**n))
+    for k in Kpluseven(n):
+        sum += Hkspace(n,k)    
+    return sum    
+    
+def Hkspaceodd(n):
+    sum = Hk0pi(n)
+    for k in Kplusodd(n):
+        sum += Hkspace(n,k)   
+    return sum
+
+#print(Hkspaceodd(3))    
+
+def H52(k):
+    return 2*kappa*J*(np.sin(2*phi)*np.sin(k)*sx + np.cos(2*phi)*np.sin(k)*sy  \
+    + (h - J*np.cos(k))*sz)
+        
+print(H52(2*np.pi/3)) 
+
+vals, vecs = np.linalg.eig(H52(2*np.pi/3))       
+print(vals)
+print(vecs)
+        
+        
+    
+    
+    
 
 
